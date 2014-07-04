544d3eb6f258f38f0fea756342a14e8401a51124
hua.zhang <hua.zhang@email.wsu.edu>
2014-07-04 14:29:04 -0700

    add singly linked list

---
+++ ch3/Node.java
@@ -0,0 +1,23 @@
+/**
+ * Node of a singly lined list of strings. 
+ * @author hua.zhang
+ *
+ */
+public class Node {
+	private String element;			// we assume elements are character strings
+	private Node next;
+	/** Creates a node with the given element and next node. */
+	public Node(String s, Node n) {
+		element = s;
+		next = n;
+	}
+	/** Returns the element of this node. */
+	public String getElement() { return element; }
+	/** Returns the next  node of this node. */
+	public Node getNext() { return next; }
+	//Modifier  methods:
+	/** Sets the next node of this node. */
+	public void setElement(String newElem) { element = newElem; }
+	/** Sets the next node of this node .*/
+	public void setNext(Node newNext) { next = newNext ; }
+}
