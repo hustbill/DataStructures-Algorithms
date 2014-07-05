9e3568e4cbe2a39808a7e3935dccf6f824855964
hua.zhang <hua.zhang@email.wsu.edu>
2014-07-04 17:03:59 -0700

    add doubly lined list

---
+++ ch3/DNode.java
@@ -0,0 +1,28 @@
+/**
+ *  Node of a doubly linked list of string
+ * @author hua.zhang
+ *
+ */
+public class DNode {
+	protected String element;		// String element stored by a node
+	protected DNode next, prev;		// Pointers to next and previous nodes
+	/** Constructor that creates a node with given fields */
+	public DNode( String e, DNode p, DNode n) {
+		element = e;
+		prev = p;
+		next = n;
+	}
+	/** Returns  the element of this node */
+	public String getElement() { return element; }
+	/** Returns the previous node of this node */
+	public DNode getPrev() { return prev; }
+	/** Returns the next node of this node */
+	public DNode getNext() { return next; }
+	/** Set the element of this node */
+	public void setElement(String newElem) { element = newElem; }
+	/** Set the previous node of this node */
+	public void setPrev(DNode newPrev) { prev = newPrev; }
+	/** Set the next node of this node */
+	public void setNext(DNode newNext) { next = newNext; }
+	
+}
