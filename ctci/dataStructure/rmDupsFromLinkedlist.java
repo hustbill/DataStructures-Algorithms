7e8e79157540bb7d20c3ffffb5576bb33991207d
hua.zhang <billyzhang2010@gmail.com>
2014-07-22 16:00:28 -0700

    add ctci solution files

---
+++ ctci/dataStructure/rmDupsFromLinkedlist.java
@@ -0,0 +1,13 @@
+package dataStructure;
+
+//2.1 remove duplicates from an unsorted linked list
+public class rmDupsFromLinkedlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	Node
+
+}
