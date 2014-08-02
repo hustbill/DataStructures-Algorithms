63572669389d979ebcc9d857c472e7335b64fc1d
hua.zhang <billyzhang2010@gmail.com>
2014-08-02 16:27:07 -0700

    update the return expression in SameTree.java

--- onlineJudge2/SameTree.java
+++ onlineJudge2/SameTree.java
@@ -54,22 +54,11 @@ public class SameTree {
     public static boolean isSameTree(TreeNode p, TreeNode q) {
     	if (p == null && q == null)
             return true;
-    	if ((p == null && q != null) || (p !=null && q == null)) 
-    		return false;
-    	
-    	
-    	if( isSameTree(p.left, q.left) == false) {
-    		return false;
-    	}
-    	
-    	if( isSameTree(q.right, q.right) == false) {
-    		return false;
-    	}
-    		
+    	if (p == null || q == null) 
+    		return false;    		
     	if (p.val != q.val) 
     		return false;
-        return true;
-            
+        return  (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));                
       }
 
 }
